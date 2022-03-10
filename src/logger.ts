89eeca278e6e63e6a9039496ee656d80bc773d8f
vialoh <github@vialoh.me>
2022-03-11 00:49:22 +0100

    use loglevel
    Molecule: appLanguage=TypeScript, appLogging=Loglevel
    (cherry picked from commit 416c172723179e9ee300b9d67d465d4cad634bb1)

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,31 @@
 /**
- * The logger, using the native `console`.
+ * The logger, using `loglevel`.
  * 
- * If you have more complex logging requirements, feel free to swap out `console` with something else in this module.
+ * If you have more complex logging requirements, feel free to swap out `loglevel` with something else in this module.
+ * 
+ * @see https://www.npmjs.com/package/loglevel
  * 
  * @module
  */
 
-const logger = console
+import logger from 'loglevel'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    export interface ProcessEnv {
+      /**
+       * The log level.
+       *
+       * @default warn
+       */
+      REACT_APP_LOG_LEVEL?: `trace` | `debug` | `info` | `warn` | `error` | `silent`
+    }
+  }
+}
+
+if (process.env.REACT_APP_LOG_LEVEL) {
+  logger.setLevel(process.env.REACT_APP_LOG_LEVEL)
+}
 
 export { logger }
